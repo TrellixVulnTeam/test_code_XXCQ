1a87675830325edf62c8d26c609a9b4aa7ed6eea
cwang <cwang@matterport.com>
2019-08-17 21:17:41 -0700

    change thread_mutex

--- cpp_test/thread_mutex/thread_mutex.cpp
+++ cpp_test/thread_mutex/thread_mutex.cpp
@@ -6,6 +6,8 @@
 #include <chrono>
 #include <functional>
 
+std::mutex mutex_;
+
 /*!
  * A class that has thread object as member variable
  */
@@ -13,7 +15,7 @@ class ThreadWrapper
 {
 private:
     //! std::thread object
-    std::thread threadHandler;
+    std::thread thread_;
 
 public:
     //! Delete the default copy constructor
@@ -23,7 +25,7 @@ public:
     ThreadWrapper& operator=(const ThreadWrapper&) = delete;
 
     //! Parameterized Constructor
-    ThreadWrapper(std::function<void()> func);
+    ThreadWrapper(std::function<void()> thread_func);
 
     //! Define Move Constructor (move input object to current object)
     ThreadWrapper(ThreadWrapper&& obj);
@@ -36,10 +38,14 @@ public:
 };
 
 //! Parameterized Constructor
-ThreadWrapper::ThreadWrapper(std::function<void()> func) : threadHandler(func) {}
+ThreadWrapper::ThreadWrapper(std::function<void()> thread_func) : thread_(thread_func)
+{
+    std::cout << "Initialization. " << std::endl;
+    std::cout << "Yes" << std::endl;
+}
 
 //! Move Constructor
-ThreadWrapper::ThreadWrapper(ThreadWrapper&& obj) : threadHandler(std::move(obj.threadHandler))
+ThreadWrapper::ThreadWrapper(ThreadWrapper&& obj) : thread_(std::move(obj.thread_))
 {
     std::cout << "Move Constructor is called" << std::endl;
 }
@@ -48,25 +54,30 @@ ThreadWrapper::ThreadWrapper(ThreadWrapper&& obj) : threadHandler(std::move(obj.
 ThreadWrapper& ThreadWrapper::operator=(ThreadWrapper&& obj)
 {
     std::cout << "Move Assignment is called" << std::endl;
-    if (threadHandler.joinable())
-        threadHandler.join();
-    threadHandler = std::move(obj.threadHandler);
+    if (thread_.joinable())
+        thread_.join();
+    thread_ = std::move(obj.thread_);
     return *this;
 }
 
 // Destructor
 ThreadWrapper::~ThreadWrapper()
 {
-    if (threadHandler.joinable())
-        threadHandler.join();
+    if (thread_.joinable())
+        thread_.join();
 }
 
 int main()
 {
-    // Creating a std::function object
-    std::function<void()> func = []() {
+    // Creating a std::function object for thread usage
+    std::function<void()> thread_func = []() {
+        std::lock_guard<std::mutex> lock(mutex_);
+
+
         // Sleep for 1 second
         std::this_thread::sleep_for(std::chrono::seconds(1));
+
+
         // Print thread ID
         std::cout << "From Thread ID : " << std::this_thread::get_id() << "\n";
     };
@@ -74,22 +85,22 @@ int main()
     {
         // Create a ThreadWrapper object
         // It will internally start the thread
-        ThreadWrapper wrapper(func);
+        ThreadWrapper wrapper(thread_func);
         // When wrapper will go out of scope, its destructor will be called
         // Which will internally join the member thread object
     }
     // Create a vector of ThreadWrapper objects
-    std::vector<ThreadWrapper> vecOfThreads;
+    std::vector<ThreadWrapper> thread_wrappers;
 
     // Add ThreadWrapper objects in thread
-    ThreadWrapper thwp1(func);
-    ThreadWrapper thwp2(func);
-    vecOfThreads.push_back(std::move(thwp1));
-    vecOfThreads.push_back(std::move(thwp2));
-    ThreadWrapper thwp3(func);
+    ThreadWrapper thread_wrapper1(thread_func);
+    ThreadWrapper thread_wrapper2(thread_func);
+    thread_wrappers.push_back(std::move(thread_wrapper1));
+    thread_wrappers.push_back(std::move(thread_wrapper2));
+    ThreadWrapper thread_wrapper3(thread_func);
 
     // Change the content of vector
-    vecOfThreads[1] = std::move(thwp3);
+    thread_wrappers[1] = std::move(thread_wrapper3);
 
     // When vector will go out of scope, its destructor will be called, which will
     // internally call the destructor all ThreadWrapper objects , which in turn
