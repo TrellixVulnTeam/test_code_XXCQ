0a87757b7d6b64083d5c095b61c5c0bcc151aa20
cwang <cwang@matterport.com>
2019-08-23 20:38:02 -0700

    add some code of decorator pattern

--- design_patterns/design_patterns/decorator.h
+++ design_patterns/design_patterns/decorator.h
@@ -1,3 +1,11 @@
+/*
+
+The Decorator Pattern:
+- Open-Close principle: Classes should be open for extension but closed for modification.
+- Decorator attaches additional responsibilities to an object dynamically.
+- The Decorator Pattern provides an alternative to subclassing for extending behavior.
+*/
+
 #pragma once
 
 #include <iostream>
@@ -20,11 +28,11 @@ protected:
     std::string description_;
 };
 
-//! Derived class: concrete component
+//! Derived class: concrete component from base
 class HouseBlend : public Coffee
 {
 public:
-    HouseBlend() { description_ += ": HouseBlend"; }
+    HouseBlend() { description_ += " - HouseBlend"; }
 
     ~HouseBlend() { std::cout << "~HouseBlend()" << std::endl; }
 
@@ -33,11 +41,11 @@ public:
     double cost() override { return 1.99; }
 };
 
-//! Derived class: concrete component
+//! Derived class: concrete component from base
 class DarkRoast : public Coffee
 {
 public:
-    DarkRoast() { description_ += ": DarkRoast"; }
+    DarkRoast() { description_ += " - DarkRoast"; }
 
     ~DarkRoast() { std::cout << "~DarkRoast()" << std::endl; }
 
@@ -46,20 +54,54 @@ public:
     double cost() override { return 2.99; }
 };
 
+//--------------------------------------------------------------------------------------
 //! Decorator base is a derived class of the abstract component base
 class Decorator : public Coffee
 {
 public:
-    Decorator() { description_ = "Decorator";  }
+    Decorator() {}
 
     ~Decorator() { std::cout << "~Decorator()" << std::endl; }
 
-	virtual std::string getDescription() = 0;
-	
-	virtual double cost() = 0;
+    virtual std::string getDescription() = 0;
+
+    virtual double cost() = 0;
+};
+
+//! Derived concrete class from Decorator
+class Milk : public Decorator
+{
+public:
+    Milk(std::unique_ptr<Coffee>& ptr) : coffee_ptr_(std::move(ptr)) {}
+
+    ~Milk() { std::cout << "~Milk()" << std::endl; }
+
+    std::string getDescription() { return coffee_ptr_->getDescription() + ", Decorator - Milk"; }
+
+    double cost() { reteurn coffee_ptr_->cost() + "0.59"; }
+
+private:
+    std::unique_ptr<Coffee> coffee_ptr_;
+};
+
+//! Derived concrete class from Decorator
+class Mocha : public Decorator
+{
+public:
+    Mocha(std::unique_ptr<Coffee>& ptr) : coffee_ptr_(std::move(ptr)) {}
+
+    ~Mocha() { std::cout << "~Mocha()" << std::endl; }
+
+    std::string getDescription() { return coffee_ptr_->getDescription() + ", Decorator - Mocha"; }
+
+    double cost() { reteurn coffee_ptr_->cost() + "1.20"; }
+
+private:
+    std::unique_ptr<Coffee> coffee_ptr_;
 };
 
 inline void testDecorator()
 {
+    std::unique_ptr<Coffee> coffee_ptr = std::make_unique<HouseBlend>();
 
 }
